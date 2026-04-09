d83b2e00c1639806de98f279a42ed0dd9f9b80c2
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-04-10 00:55:45 +0600

    solve 1 qtn

--- set2.py
+++ set2.py
@@ -1,2 +1,11 @@
-print("Hello, World!")
-print('hi')
+# print("Hello, World!")
+# print('hi')
+# 1. Positive or negative number: input an integer, print whether it’s positive, negative, or
+# zero.
+number = int(input("enter any number"))
+if number>0:
+    print("possitive number")
+elif number<0:
+    print("negetive number")
+else:
+     print("zero")
