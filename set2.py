3d0f8affd6a5823e6a8f204c26aff3358758b047
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-04-10 01:17:16 +0600

    solve number 3

--- set2.py
+++ set2.py
@@ -1,17 +1,36 @@
-# # # print("Hello, World!")
-# # # print('hi')
-# # # 1. Positive or negative number: input an integer, print whether it’s positive, negative, or
-# # # zero.
-# # number = int(input("enter any number"))
-# # if number>0:
-# #     print("possitive number")
-# # elif number<0:
-# #     print("negetive number")
-# # else:
-# #      print("zero")
-# Even or odd: check if a number is divisible by 2.
-number = int(input("any number = "))
-if number % 2 == 0:
-    print("even number") 
+# # # # print("Hello, World!")
+# # # # print('hi')
+# # # # 1. Positive or negative number: input an integer, print whether it’s positive, negative, or
+# # # # zero.
+# # # number = int(input("enter any number"))
+# # # if number>0:
+# # #     print("possitive number")
+# # # elif number<0:
+# # #     print("negetive number")
+# # # else:
+# # #      print("zero")
+# # Even or odd: check if a number is divisible by 2.
+# number = int(input("any number = "))
+# if number % 2 == 0:
+#     print("even number") 
+# else:
+#     print("odd number")
+
+# 3. Largest of two numbers: compare two numbers using if / else.
+#  number1 = float(input("enter first number = "))
+#  number2 = float(input("enter second number = "))
+#  if number1> number2:
+#     print("number1 is big number",number1)
+#  else:
+#     print("number2 is a big number",number2)
+# another
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if num1 > num2:
+    print("The largest number is:", num1)
 else:
-    print("odd number")
+    print("The largest number is:", num2)
+
+
+
