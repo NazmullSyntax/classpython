08778046edc22f22a550ac72bda90693b1fee444
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-04-10 01:00:11 +0600

    solve 2 qtn

--- set2.py
+++ set2.py
@@ -1,11 +1,17 @@
-# print("Hello, World!")
-# print('hi')
-# 1. Positive or negative number: input an integer, print whether it’s positive, negative, or
-# zero.
-number = int(input("enter any number"))
-if number>0:
-    print("possitive number")
-elif number<0:
-    print("negetive number")
+# # # print("Hello, World!")
+# # # print('hi')
+# # # 1. Positive or negative number: input an integer, print whether it’s positive, negative, or
+# # # zero.
+# # number = int(input("enter any number"))
+# # if number>0:
+# #     print("possitive number")
+# # elif number<0:
+# #     print("negetive number")
+# # else:
+# #      print("zero")
+# Even or odd: check if a number is divisible by 2.
+number = int(input("any number = "))
+if number % 2 == 0:
+    print("even number") 
 else:
-     print("zero")
+    print("odd number")
