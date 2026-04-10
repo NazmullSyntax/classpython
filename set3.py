91c8b55f989ec4b3336b76afc35636806380a32e
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-04-11 00:34:46 +0600

    solve 5

--- set3.py
+++ set3.py
@@ -1,28 +1,38 @@
-# # # print("welcome to set3.py")
-# # #     1. Print numbers from 1 to N: simple counting loop
+# # # # print("welcome to set3.py")
+# # # #     1. Print numbers from 1 to N: simple counting loop
+
+# # # n = int(input("Enter a number: "))
+
+# # # for i in range(1, n + 1):
+# # #     print(i)
+
+# # #     2. Print numbers from N down to 1: reverse counting.
 
 # # n = int(input("Enter a number: "))
 
-# # for i in range(1, n + 1):
+# # for i in range(n, 0, -1):
 # #     print(i)
 
-# #     2. Print numbers from N down to 1: reverse counting.
+# # 3. Print even numbers from 1 to N.
 
 # n = int(input("Enter a number: "))
 
-# for i in range(n, 0, -1):
+# for i in range(2, n + 1, 2):
 #     print(i)
 
-# 3. Print even numbers from 1 to N.
+#     # 4. Print odd numbers from 1 to N.
+# n = int(input("Enter a number: "))
 
+# for i in range(1, n + 1, 2):
+#     print(i)
+
+#     5. Print sum of first N natural numbers.
 n = int(input("Enter a number: "))
 
-for i in range(2, n + 1, 2):
-    print(i)
+total = 0
 
-    # 4. Print odd numbers from 1 to N.
-n = int(input("Enter a number: "))
+for i in range(1, n + 1):
+    total += i
 
-for i in range(1, n + 1, 2):
-    print(i)
+print("Sum =", total)
 
