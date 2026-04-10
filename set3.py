1ea7baeeae26aadb3a4619b3773c757aedcee1eb
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-04-11 00:49:49 +0600

    solve 8

--- set3.py
+++ set3.py
@@ -1,57 +1,63 @@
-# # # # # print("welcome to set3.py")
-# # # # #     1. Print numbers from 1 to N: simple counting loop
+# # # # # # # print("welcome to set3.py")
+# # # # # # #     1. Print numbers from 1 to N: simple counting loop
+
+# # # # # # n = int(input("Enter a number: "))
+
+# # # # # # for i in range(1, n + 1):
+# # # # # #     print(i)
+
+# # # # # #     2. Print numbers from N down to 1: reverse counting.
+
+# # # # # n = int(input("Enter a number: "))
+
+# # # # # for i in range(n, 0, -1):
+# # # # #     print(i)
+
+# # # # # 3. Print even numbers from 1 to N.
 
 # # # # n = int(input("Enter a number: "))
 
-# # # # for i in range(1, n + 1):
+# # # # for i in range(2, n + 1, 2):
 # # # #     print(i)
 
-# # # #     2. Print numbers from N down to 1: reverse counting.
+# # # #     # 4. Print odd numbers from 1 to N.
+# # # # n = int(input("Enter a number: "))
 
+# # # # for i in range(1, n + 1, 2):
+# # # #     print(i)
+
+# # # #     5. Print sum of first N natural numbers.
 # # # n = int(input("Enter a number: "))
 
-# # # for i in range(n, 0, -1):
-# # #     print(i)
+# # # total = 0
 
-# # # 3. Print even numbers from 1 to N.
+# # # for i in range(1, n + 1):
+# # #     total += i
 
+# # # print("Sum =", total)
+
+# # # 6. Print sum of even numbers from 1 to N.
 # # n = int(input("Enter a number: "))
+
+# # total = 0
 
 # # for i in range(2, n + 1, 2):
-# #     print(i)
+# #     total += i
 
-# #     # 4. Print odd numbers from 1 to N.
+# # print("Sum of even numbers =", total)
+
+# # # 7. Print sum of odd numbers from 1 to N.
 # # n = int(input("Enter a number: "))
-
+# # total = 0
 # # for i in range(1, n + 1, 2):
-# #     print(i)
+# #     total += i
 
-# #     5. Print sum of first N natural numbers.
-# n = int(input("Enter a number: "))
+# # print("Sum of odd numbers =", total)
 
-# total = 0
+# 8. Print multiplication table for any number.
+n = int(input("enter value"))
 
-# for i in range(1, n + 1):
-#     total += i
-
-# print("Sum =", total)
-
-# 6. Print sum of even numbers from 1 to N.
-n = int(input("Enter a number: "))
-
-total = 0
-
-for i in range(2, n + 1, 2):
-    total += i
-
-print("Sum of even numbers =", total)
-
-# 7. Print sum of odd numbers from 1 to N.
-n = int(input("Enter a number: "))
-total = 0
-for i in range(1, n + 1, 2):
-    total += i
-
-print("Sum of odd numbers =", total)
+for i in range(1, 11):
+    print(n * i)
 
 
