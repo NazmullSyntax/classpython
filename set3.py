b83c75c28b50a8890e74a8f70842be3cb669697f
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-04-11 00:22:14 +0600

    solve 1

--- set3.py
+++ set3.py
@@ -1,1 +1,7 @@
-print("welcome to set3.py")
+# print("welcome to set3.py")
+#     1. Print numbers from 1 to N: simple counting loop
+
+n = int(input("Enter a number: "))
+
+for i in range(1, n + 1):
+    print(i)
